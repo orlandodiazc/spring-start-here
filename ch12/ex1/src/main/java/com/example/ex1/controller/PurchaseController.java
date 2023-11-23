08336fffb117828122ac9f7695d08f341deed34d
Orlando Diaz <orlandodiazconde@gmail.com>
2023-11-23 18:23:08 -0500

    Add ch12 exercises (using data sources)

---
+++ ch12/ex1/src/main/java/com/example/ex1/controller/PurchaseController.java
@@ -0,0 +1,26 @@
+package com.example.ex1.controller;
+
+import com.example.ex1.model.Purchase;
+import com.example.ex1.repository.PurchaseRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/purchase")
+public class PurchaseController {
+    private final PurchaseRepository purchaseRepository;
+
+    public PurchaseController(PurchaseRepository purchaseRepository) {
+        this.purchaseRepository = purchaseRepository;
+    }
+    @PostMapping
+    public void storePurchase(@RequestBody Purchase purchase) {
+        purchaseRepository.storePurchase(purchase);
+    }
+
+    @GetMapping
+    public List<Purchase> findPurchases() {
+        return purchaseRepository.findAllPurchases();
+    }
+}
---
+++ ch12/ex3/src/main/java/com/example/ex3/controller/PurchaseController.java
@@ -0,0 +1,26 @@
+package com.example.ex3.controller;
+
+import com.example.ex3.model.Purchase;
+import com.example.ex3.repository.PurchaseRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/purchase")
+public class PurchaseController {
+    private final PurchaseRepository purchaseRepository;
+
+    public PurchaseController(PurchaseRepository purchaseRepository) {
+        this.purchaseRepository = purchaseRepository;
+    }
+    @PostMapping
+    public void storePurchase(@RequestBody Purchase purchase) {
+        purchaseRepository.storePurchase(purchase);
+    }
+
+    @GetMapping
+    public List<Purchase> findPurchases() {
+        return purchaseRepository.findAllPurchases();
+    }
+}
